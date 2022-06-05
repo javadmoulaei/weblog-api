001bdfe02ccfac15bd029aa27679625b14d3221e
Javad Moulaei <ijavadmoulaei@gmail.com>
2022-06-05 13:24:59 +0430

    develop headers

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const fileUpload = require("express-fileupload");
 
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errors");
+const { setHeaders } = require("./middleware/headers");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -21,6 +22,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(setHeaders);
 
 app.use(fileUpload());
 
