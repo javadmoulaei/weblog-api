b708491bfec5fac396d5a822eb4bf248a199d525
Javad Moulaei <ijavadmoulaei@gmail.com>
2022-06-27 17:08:32 +0430

    refactore: auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,25 @@
-exports.authenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
+const jwt = require("jsonwebtoken");
 
-  res.redirect("/404");
+const User = require("../models/User");
+
+exports.authenticated = async (req, res, next) => {
+  try {
+    let token = req.get("Authorization") || "";
+    token = token.split(" ")[1]; //Bearer Token
+
+    if (!token) return res.status(403).send({ message: "دسترسی ندارید" });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) return res.status(403).send({ message: "دسترسی ندارید" });
+
+    const user = await User.findOne({ _id: decodedToken.userId });
+
+    if (!user) return res.status(403).send({ message: "دسترسی ندارید" });
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return helpers.res.serErr(res, error);
+  }
 };
